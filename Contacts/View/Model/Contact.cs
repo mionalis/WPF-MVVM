cf821f6483ec3be781f24ebe7c7f0d415c34946a
mionalis <mionallis@gmail.com>
2023-09-09 22:08:37 +0700

    feature(Model): добавлен класс Contact

---
+++ Contacts/View/Model/Contact.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View.Model
+{
+    internal class Contact
+    {
+        private string _fullName;
+
+        private string _email;
+
+        private int _phoneNumber;
+
+        public Contact(string fullName, string email, int phoneNumber)
+        {
+            FullName = fullName;
+            Email = email;
+            PhoneNumber = phoneNumber;
+        }
+
+        public Contact()
+        {
+
+        }
+
+        public string FullName 
+        {
+            get { return _fullName; }
+
+            set 
+            {
+                _fullName = value; 
+            }
+        }
+
+        public string Email 
+        {
+            get { return _email; } 
+
+            set
+            { 
+                _email = value; 
+            }
+        }
+
+        public int PhoneNumber 
+        { 
+            get { return _phoneNumber; }
+
+            set
+            {
+                _phoneNumber = value;
+            }
+        }
+    }
+}
