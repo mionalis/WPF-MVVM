7adc5e32cd64d31096eb64cfbdd25b8b92568175
mionalis <mionallis@gmail.com>
2023-09-14 21:15:25 +0700

    feature(ViewModel): добавлен класс MainMV

---
+++ Contacts/View/ViewModel/MainVM.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Common;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+using View.Model;
+
+namespace View.ViewModel
+{
+    internal class MainVM : INotifyPropertyChanged
+    {
+        public Contact Contact { get; set; } = new Contact();
+
+        public string FullName
+        {
+            get => Contact.FullName;
+            set
+            {
+                Contact.FullName = value;
+                OnPropertyChanged(nameof(FullName));
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get => Contact.PhoneNumber;
+            set
+            {
+                Contact.PhoneNumber = value;
+                OnPropertyChanged(nameof(PhoneNumber));
+            }
+        }
+
+        public string Email
+        {
+            get => Contact.Email;
+            set
+            {
+                Contact.Email = value;
+                OnPropertyChanged(nameof(Email));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
