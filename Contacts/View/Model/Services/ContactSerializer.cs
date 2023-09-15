74923af39e30368a494515322b3b0571dba32901
mionalis <mionallis@gmail.com>
2023-09-15 18:59:06 +0700

    style(ContactSerializer): добавлены XML-комментарии

--- Contacts/View/Model/Services/ContactSerializer.cs
+++ Contacts/View/Model/Services/ContactSerializer.cs
@@ -8,17 +8,31 @@ using Newtonsoft.Json;
 
 namespace View.Model.Services
 {
+    /// <summary>
+    /// Выполняет сохранение и загрузку одного объекта контакта из файла.
+    /// </summary>
     internal static class ContactSerializer
     {
+        /// <summary>
+        /// Возвращает путь файла, в котором будут храниться сериализованные данные.
+        /// </summary>
         private static string Path =>
             Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\contacts.json";
 
+        /// <summary>
+        /// Сохраняет объект контакта в файл по заданному пути.
+        /// </summary>
+        /// <param name="contact">Контакт.</param>
         public static void Save(Contact contact)
         {
             var json = JsonConvert.SerializeObject(contact, Formatting.Indented);
             File.WriteAllText(Path, json);
         }
 
+        /// <summary>
+        /// Выполняет загрузку объекта контакта из файла по заданному пути. 
+        /// </summary>
+        /// <returns> Объект контакта.</returns>
         public static Contact Load()
         {
             var contact = new Contact();
