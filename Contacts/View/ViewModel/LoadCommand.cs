05a03c1cd5d07e58803003c9638e7073603af263
mionalis <mionallis@gmail.com>
2023-09-15 19:34:17 +0700

    feature(ViewModel): добавлены классы LoadCommand и SaveCommand

---
+++ Contacts/View/ViewModel/LoadCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using View.Model.Services;
+using View.Model;
+
+namespace View.ViewModel
+{
+    internal class LoadCommand : ICommand
+    {
+        public Contact Contact { get; set; }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            Contact = ContactSerializer.Load();
+        }
+    }
+}
